86bc98e75e9d575528d73463526049af18f1cd1f
Kyle <KyleMit@gmail.com>
2015-12-02 16:37:31 -0500

    Add methods

--- MeteorDemo/client/question-submit.js
+++ MeteorDemo/client/question-submit.js
@@ -4,10 +4,11 @@ Template.question_submit.events({
 		var txt = ev.target.question.value;
 
 		// insert into database
-		Questions.insert({
+		var question = {
 			text: txt,
 			author: Meteor.user().username
-		});
+		}
+		Meteor.call('insertQuestion', question)
 
 		// clean up after ourselves
 		ev.target.question.value = "";
--- MeteorDemo/lib/collections.js
+++ MeteorDemo/lib/collections.js
@@ -7,9 +7,16 @@ if (Meteor.isServer) {
 
 	// only seed empty database
 	if (Questions.find().count() === 0) {
-		
+
 	  Questions.insert(
 	    {text: "Seed Question?"}
 	  );
 	}	
 }
+
+
+Meteor.methods({
+	'insertQuestion': function(q) {
+		Questions.insert(q)
+	}
+});
